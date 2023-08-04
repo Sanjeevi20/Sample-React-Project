b585f97453ecea164654b8b49703139b09916b16
Sanjeevi20 <nsanjeevi03@gmail.com>
2023-08-04 10:28:03 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Table from './components/Table.js'
+import Moose from './components/moose.js'
+import Note from './components/Note.js'
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+      <Moose></Moose>
+      <Note></Note>
+    </div>
+  );
+}
+
+export default App;
